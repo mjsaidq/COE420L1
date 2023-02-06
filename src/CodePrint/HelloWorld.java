e954c8248feb304a10ee2f84497b08c203b380e7
junaid <b00089812@aus.edu>
2023-02-06 17:14:43 +0400

    Initial Commit

---
+++ src/CodePrint/HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package CodePrint;
+
+/**
+ * @author b00089812
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
