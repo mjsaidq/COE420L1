c070adc658dc40a865af370207dcff9aa381daf3
junaid <b00089812@aus.edu>
2023-02-06 18:17:30 +0400

    Feature N Added

--- src/CodePrint/HelloWorld.java
+++ src/CodePrint/HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello AWEOMEEE World");
 		// TODO Auto-generated method stub
 
 	}
